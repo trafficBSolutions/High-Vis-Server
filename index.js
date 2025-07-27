d5aee7ececc4e65877778fd5be59baaa35c71ba1
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-07-27 15:47:51 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+
+// Create Express app
+const app = express();
+
+// Database connection
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log('Database Connected');
+        // Call removeDuplicates function after database connection
+        
+    })
+    .catch((err) => console.log('Database Not Connected', err));
+
+// Middleware
+app.use(express.json());
+
+
+// Routes
+app.use('/', require('./routes/testRoute'));
+app.use('/', require('./routes/contactRoute'));
+app.use('/', require('./routes/serviceRoute'))
+// Define port
+const port = process.env.PORT || 8000;
+
+// Start server
+app.listen(port, '0.0.0.0', () => console.log(`Server is running on port ${port}`));
