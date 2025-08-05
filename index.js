67fbff51169239126d006100a2db5e7342f52bc1
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-08-05 10:57:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-const xss = require('xss-clean');
 const compression = require('compression');
 const cors = require('cors');
 const sanitizeHtml = require('sanitize-html');
@@ -12,7 +11,6 @@ const app = express();
 
 // ✅ Security Middleware
 app.use(helmet()); // Adds secure HTTP headers
-app.use(xss()); // Prevent XSS
 app.use(compression()); // GZIP compression
 app.use((req, res, next) => {
   if (req.body) {
@@ -32,10 +30,22 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
-// ✅ CORS Config
+const allowedOrigins = [
+  'https://www.highvisibilitydetailing.com',
+  'http://localhost:3000'
+];
+
 app.use(cors({
-  credentials: true,
-  origin: ['https://www.highvisibilitydetailing.com'] // Adjust as needed
+  origin: function (origin, callback) {
+    // allow requests with no origin like mobile apps or curl
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
 
 // ✅ Parse incoming JSON
